bc89181fb769bee9f4cd79166e0cbe761d81ff75
redaaa <reda@dockflow.com>
2020-11-26 15:21:14 +0100

    changes

---
+++ app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+ 
+dotenv.config()
+const port = process.env.PORT;
+const mongoUrl = process.env.MONGO_URL;
+
+console.log(process.env.PORT);
+
+const app = express();
+
+// mongoose.connect('', {useNewUrlParser: true, useUnifiedTopology: true});
+
+// mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+//body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+});
+
+module.exports = app;
--- build/app.js
+++ build/app.js
@@ -1,12 +1,25 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-// lib/app.ts
-var express = require("express");
-// Create a new express application instance
-var app = express();
+var express_1 = __importDefault(require("express"));
+var body_parser_1 = __importDefault(require("body-parser"));
+var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+var port = process.env.PORT;
+var mongoUrl = process.env.MONGO_URL;
+console.log(process.env.PORT);
+var app = express_1.default();
+// mongoose.connect('', {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+//body parser
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.get('/', function (req, res) {
-    res.send('Hello Dockflow!');
+    res.send('Hello World!');
 });
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+    console.log("Example app listening on port " + port + "!");
 });
+module.exports = app;
