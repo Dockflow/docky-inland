c2135fe7ab6acbf2133189649c7d9e78b910f472
Joren lauryssen <joren.etw@gmail.com>
2020-11-27 10:20:40 +0100

    added query string support

--- controllers/routeController.ts
+++ controllers/routeController.ts
@@ -3,7 +3,8 @@ import * as locks from './routes.json';
 
 export const routeController = {
     getRoute :async(req:Request, res:Response) => {
-        console.log(req);
-        return res.status(200).json(locks);
+        let type:string = req.query.t as string;
+        if(!type){ type = 'anrpar' };
+        return res.status(200).json((locks[0] as any)[type]);
     }
 }
